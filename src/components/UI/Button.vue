593da932ffda451ff2b7cc2ea711ed4e768f5b25
吴成 <1965417564@qq.com>
2023-08-24 09:47:00 +0800

    feat(UI):新增Button组件

---
+++ src/components/UI/Button.vue
@@ -0,0 +1,21 @@
+<template>
+  <n-button type="primary" @click="$emit('click')">
+    <slot>Button</slot>
+  </n-button>
+</template>
+<script lang="ts" setup>
+import { PropType } from 'vue'
+type ButtonType = 'primary' | 'success' | 'warning' | 'error' | 'info' | 'tertiary'
+
+defineProps({
+  /**
+   * 按钮类型
+   */
+  type: {
+    type: String as PropType<ButtonType>,
+    default: 'primary',
+  },
+})
+// eslint-disable-next-line no-unused-vars
+defineEmits<{(event: 'click', data: void): void;}>()
+</script>
