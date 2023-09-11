ca002d2dd0dd45dbc944ea230461c683afb140ca
吴成 <1965417564@qq.com>
2023-09-11 17:29:33 +0800

    feat(表单组件):新增Select组件

---
+++ src/components/Form/formItem/Select.vue
@@ -0,0 +1,96 @@
+<template>
+  <n-select
+    v-model:value="value"
+    filterable
+    value-field="key"
+    label-field="label"
+    placeholder="选择歌曲"
+    max-tag-count="responsive"
+    :disabled="props.disabled"
+    :multiple="props.multiple"
+    :clearable="fieldConfig?.clearable"
+    :render-option="renderOption"
+    :options="(enumRecord as unknown as SelectMixedOption[]) "
+  />
+</template>
+<script lang="ts" setup>
+import { ref, watch, PropType } from 'vue'
+import { AnFormFieldProps } from '@/model/basic/AnFormFieldProps'
+import { AnFormFieldConfig } from '@/model/fieldConfig/AnFormFieldConfig'
+import { InRecord } from '@/interface/base/InRecord'
+import { SelectMixedOption } from 'naive-ui/es/select/src/interface'
+import { RenderOption } from 'naive-ui/es/dropdown/src/interface'
+
+const emits = defineEmits(['update:modelValue'])
+const props = defineProps({
+  ...AnFormFieldProps(),
+  /**
+   * # 枚举值翻译，用于渲染选项 优先级大于fieldConfig.emumRecord
+   */
+  enumRecord: {
+    type: Array as PropType<InRecord[]>,
+    default: undefined,
+  },
+  /**
+   * # 是否多选
+   */
+  multiple: {
+    type: Boolean,
+    default: false,
+  },
+  /**
+   * # 选项的渲染函数
+   */
+  renderOption: {
+    type: Function as PropType<RenderOption>,
+    default: undefined,
+  },
+})
+
+watch(
+  () => props.modelValue,
+  (newValue) => {
+    value.value = (newValue as Array<string | number> | string | number | null) ?? ''
+  },
+  {
+    deep: true,
+  },
+)
+watch(
+  () => value.value,
+  (newValue) => {
+    emits('update:modelValue', newValue)
+  },
+  {
+    deep: true,
+  },
+)
+/**
+ * 绑定数据
+ */
+const value = ref<Array<string | number> | string | number | null>('')
+
+/**
+ * 字段的表单配置信息
+ */
+const fieldConfig = ref<AnFormFieldConfig | null>(null)
+
+/**
+ * 初始化
+ */
+function init () {
+  const fieldName = ref('')
+  for (const key in props.modelModifiers) {
+    fieldName.value = key
+  }
+  if (props.entity && fieldName.value) {
+    fieldConfig.value = props.entity.getCustomFormFieldConfig(fieldName.value)
+  }
+  if (props.modelValue === undefined && fieldConfig.value?.defaultValue !== undefined) {
+    // 没有初始化的值 但装饰器设置了默认值
+    value.value = fieldConfig.value.defaultValue
+    emits('update:modelValue', value.value)
+  }
+}
+init()
+</script>
