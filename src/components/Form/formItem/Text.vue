00abb648e0b2e8abf5555fd94ea33c5aabd9bdce
吴成 <1965417564@qq.com>
2023-09-11 17:28:09 +0800

    feat(表单组件):新增Text组件

---
+++ src/components/Form/formItem/Text.vue
@@ -0,0 +1,91 @@
+<template>
+  <n-input
+    v-model:value="value" 
+    :round="props.round"
+    :disabled="props.disabled"
+    :placeholder="placeholderComputed"
+    :clearable="fieldConfig?.clearable"
+    :maxlength="fieldConfig?.maxLength"
+    :minlength="fieldConfig?.minLength"
+    @on-input="emits('update:modelValue', $event.target.value)"
+  />
+</template>
+<script lang="ts" setup>
+import { ref, watch, computed } from 'vue'
+import { AnFormFieldProps } from '@/model/basic/AnFormFieldProps'
+import { AnFormFieldConfig } from '@/model/fieldConfig/AnFormFieldConfig'
+
+const emits = defineEmits(['update:modelValue'])
+const props = defineProps({
+  ...AnFormFieldProps(),
+})
+
+watch(
+  () => props.modelValue,
+  (newValue) => {
+    value.value = (newValue as string) ?? ''
+  },
+  {
+    deep: true,
+  },
+)
+watch(
+  () => value.value,
+  (newValue) => {
+    emits('update:modelValue', newValue)
+  },
+  {
+    deep: true,
+  },
+)
+
+/**
+ * 绑定数据
+ */
+const value = ref<string>('')
+
+/**
+ * 字段的表单配置信息
+ */
+const fieldConfig = ref<AnFormFieldConfig | null>(null)
+
+/**
+ * 占位符信息
+ */
+const placeholderComputed = computed(() => {
+  if (props.placeholder) {
+    return props.placeholder
+  } else if (fieldConfig.value?.placeholder) {
+    return fieldConfig.value?.placeholder
+  } else if (fieldConfig.value?.label) {
+    return `请输入${fieldConfig.value?.label}...`
+  } else if (props.entity) {
+    const fieldName = ref('')
+    for (const key in props.modelModifiers) {
+      fieldName.value = key
+    }
+    return `请输入${props.entity.getCustomFieldName(fieldName.value)}...`
+  } else {
+    return '请输入...'
+  }
+})
+
+/**
+ * 初始化
+ */
+function init () {
+  const fieldName = ref('')
+  for (const key in props.modelModifiers) {
+    fieldName.value = key
+  }
+  if (props.entity && fieldName.value) {
+    fieldConfig.value = props.entity.getCustomFormFieldConfig(fieldName.value)
+  }
+  if (props.modelValue === undefined && fieldConfig.value?.defaultValue !== undefined) {
+    // 没有初始化的值 但装饰器设置了默认值
+    value.value = fieldConfig.value.defaultValue
+    emits('update:modelValue', value.value)
+  }
+}
+init()
+</script>
---
+++ src/enum/EDateFormType.ts
@@ -0,0 +1,30 @@
+export enum EDateFormType {
+  /**
+   * # 日期
+   */
+  DATE = "date",
+  /**
+   * # 日期范围
+   */
+  DATERANGE = "daterange",
+  /**
+   * # 日期时间
+   */
+  DATETIME = "datetime",
+  /**
+   * # 日期时间范围
+   */
+  DATETIMERANGE = "datetimerange",
+  /**
+   * # 年
+   */
+  YEAR = "year",
+  /**
+   * # 月
+   */
+  MONTH = "month",  
+  /**
+   * # 月范围
+   */
+  MONTHRANGE = "monthrange",
+}
