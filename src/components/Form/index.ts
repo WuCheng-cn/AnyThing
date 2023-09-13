c76849169b42009f3cd4be0538800fd2cbd09a20
吴成 <1965417564@qq.com>
2023-09-13 11:14:13 +0800

    feat(表单组件):表单组件目录结构调整

---
+++ src/components/Form/index.ts
@@ -0,0 +1,18 @@
+import AText from './Text.vue';
+import ATextArea from './TextArea.vue';
+import ASelect from './Select.vue';
+import ACheckbox from './Checkbox.vue';
+import ARadio from './Radio.vue';
+import ADate from './Date.vue';
+import ASwitch from './Switch.vue';
+
+export {
+  AText,
+  ATextArea,
+  ASelect,
+  ACheckbox,
+  ARadio,
+  ADate,
+  ASwitch,
+};
+
