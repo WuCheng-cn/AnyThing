00313b1298a9e90ca813bbc42003d54436785de7
吴成 <1965417564@qq.com>
2023-09-18 17:06:52 +0800

    fix(git):解决git文件名大小写不敏感带来的问题

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,26 +1,25 @@
 <template>
-  <n-config-provider :theme="useConfig().AppConfig.theme">
-    <n-space vertical>
-      <n-layout style="height: 100vh;" content-style="display: flex;flex-direction: column;">
-        <n-layout-header :inverted="false" bordered>
-          <n-space justify="end" style="padding:10px">
-            <ThemeSettingVue ref="ThemeSettingRef" />
-          </n-space>
-        </n-layout-header>
-        <n-layout class="any-main">
-          <router-view />
-        </n-layout>
-      </n-layout>
-    </n-space>
-  </n-config-provider>
+  <n-layout
+    class="anything"
+    content-style="display: flex;flex-direction: column;"
+  > 
+    <n-layout-header :inverted="false" bordered>
+      <n-space justify="end" style="padding:10px">
+        <ThemeSettingVue ref="ThemeSettingRef" />
+      </n-space>
+    </n-layout-header>
+    <n-layout-content style="flex: 1;">
+      <router-view />
+    </n-layout-content>
+  </n-layout>
 </template>
 <script setup lang="ts">
 import ThemeSettingVue from './themeSetting.vue'
-import { useConfig } from '@/config'
-
 </script>
 <style lang="less" scoped>
-  .any-main{
-    flex: 1;
-  }
+.anything {
+  height: 100vh;
+  width: 100vw;
+  overflow: hidden;
+}
 </style>
