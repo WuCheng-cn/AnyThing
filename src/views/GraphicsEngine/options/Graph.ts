c9199c961ea8048353d5d49c8ca4a334bfacbd30
吴成 <1965417564@qq.com>
2023-09-19 11:56:13 +0800

    feat(Graph):节点移动范围限制

--- src/views/GraphicsEngine/options/Graph.ts
+++ src/views/GraphicsEngine/options/Graph.ts
@@ -1,4 +1,6 @@
 
+import { CellView } from "@antv/x6/lib/view/cell"
+import {Rectangle} from "@antv/x6-geometry"
 export const GraphOption:any = {
   // 启用滚轮缩放画布
   mousewheel: {
@@ -13,4 +15,28 @@ export const GraphOption:any = {
     visible: true, // 渲染网格背景
     
   },
+  translating:{
+    /**
+     * # 限制节点移动范围
+     * @param CellView 节点实例
+     * @returns 返回限制后的范围
+     * @description 限制节点在画布内移动
+     */ 
+    restrict:(CellView:CellView):Rectangle.RectangleLike=>{
+      //节点移动在画布中保持的最小宽/高
+      const minSize = 50
+      // 判断节点在即将完全移出画布时，禁止移动
+      const { x, y, width, height } = CellView.getBBox()
+      const { width: graphWidth, height: graphHeight } = CellView.graph.getGraphArea()
+      console.log(x, y, width, height, graphWidth, graphHeight);
+      
+      return{
+        x: minSize - width ,
+        y: minSize - height,
+        width: graphWidth + 2*(width - minSize),
+        height: graphHeight + 2*(height - minSize),
+      }
+      
+    },
+  }
 }
