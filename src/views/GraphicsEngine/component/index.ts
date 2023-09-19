2c8943ea1defc4a0da2eb6d3f63bd371e6ddf8c0
吴成 <1965417564@qq.com>
2023-09-19 17:36:45 +0800

    fix(bug):修复已知问题

--- src/views/GraphicsEngine/component/index.ts
+++ src/views/GraphicsEngine/component/index.ts
@@ -1,8 +1,8 @@
-import { InRegistItem } from '@/model/graph/RegistInterface';
 import Demo from './Demo/index'
 import LineChart from './LineChart/index'
 import WidgetImage from './WidgetImage/index'
 import Screen from './Screen/index'
+import { InRegistItem } from '@/interface/graph/InRegistItem'
 
 const Registry:InRegistItem[] = [
   Demo,
