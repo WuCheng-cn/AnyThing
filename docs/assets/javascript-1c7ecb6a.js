dcd774307191b8498b6e0919d6a807a74ae8925e
吴成 <1965417564@qq.com>
2024-02-04 14:00:22 +0800

    :package: build(custom): 打包发布

---
+++ docs/assets/javascript-1c7ecb6a.js
@@ -0,0 +1,6 @@
+import{conf as t,language as e}from"./typescript-c1e76050.js";import"./index-b399511d.js";import"./index-2d14f341.js";import"./vendor-a32ff1ab.js";import"./plugin-vue_export-helper-c27b6911.js";import"./Button-949af70b.js";/*!-----------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Version: 0.41.0(38e1e3d097f84e336c311d071a9ffb5191d4ffd1)
+ * Released under the MIT license
+ * https://github.com/microsoft/monaco-editor/blob/main/LICENSE.txt
+ *-----------------------------------------------------------------------------*/var c=t,l={defaultToken:"invalid",tokenPostfix:".js",keywords:["break","case","catch","class","continue","const","constructor","debugger","default","delete","do","else","export","extends","false","finally","for","from","function","get","if","import","in","instanceof","let","new","null","return","set","static","super","switch","symbol","this","throw","true","try","typeof","undefined","var","void","while","with","yield","async","await","of"],typeKeywords:[],operators:e.operators,symbols:e.symbols,escapes:e.escapes,digits:e.digits,octaldigits:e.octaldigits,binarydigits:e.binarydigits,hexdigits:e.hexdigits,regexpctl:e.regexpctl,regexpesc:e.regexpesc,tokenizer:e.tokenizer};export{c as conf,l as language};
