ec651dfee3db23bf777232452ceece8c53f9b9e3
wucheng <1965417564@qq.com>
2025-03-06 19:33:38 +0800

    :white_check_mark: test(custom): 测试

--- .release-it.js
+++ .release-it.js
@@ -1,20 +1,60 @@
 module.exports = {
-  "plugins": {
-    "@release-it/conventional-changelog": {
-      "infile": "CHANGELOG.md",
-      "preset": {
-        "config": "./changelog.config.js"
+  plugins: {
+    '@release-it/conventional-changelog': {
+      infile: 'CHANGELOG.md',
+      preset: {
+        config: {
+          parserOpts: {
+            headerPattern: '^(:[a-z]+:)\\s*(\\w+)(\\(([^)]+)\\))?\\s*:\\s*(.*)$',
+            headerCorrespondence: ['emoji', 'type', 'scope', 'scope', 'subject'],
+            noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES'],
+            issuePrefixes: ['#']
+          },
+          writerOpts: {
+            transform: (commit, context) => {
+              console.log('调试信息', commit) // 添加调试信息
+              const emojiMap = {
+                ':sparkles:': '✨',
+                ':bug:': '🐛',
+                ':tada:': '🎉',
+                ':memo:': '📝',
+                ':art:': '🎨',
+                ':recycle:': '♻️',
+                ':zap:': '⚡️',
+                ':white_check_mark:': '✅',
+                ':rewind:': '⏪️',
+                ':package:': '📦',
+                ':rocket:': '🚀',
+                ':construction_worker:': '👷'
+              }
+
+              const emoji = emojiMap[commit.emoji]
+              const type = commit.type
+              const scope = commit.scope ? `(${commit.scope})` : ''
+              const subject = commit.subject
+
+              // 构建新的提交信息
+              commit.hash = commit.hash.substring(0, 7) // 限制哈希长度
+              commit.subject = `${emoji} ${type}${scope}: ${subject}`
+
+              return commit
+            },
+            commitGroupsSort: 'title',
+            commitsSort: ['scope', 'subject'],
+            noteGroupsSort: 'title'
+          }
+        }
       }
     }
   },
-  "git": {
-    "commitMessage": ":rocket: chore(release): Release v${version}"
+  git: {
+    commitMessage: ':rocket: chore(release): Release v${version}'
   },
-  "github": {
-    "release": true,
-    "draft": false
+  github: {
+    release: true,
+    draft: false
   },
-  "npm": {
-    "publish": true
+  npm: {
+    publish: true
   }
-};
+}
