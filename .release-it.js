39f835908f98cbf5a4789f575666066b2cf45703
wucheng <1965417564@qq.com>
2025-03-06 16:53:08 +0800

    :white_check_mark: test(custom): 测试release配置

--- .release-it.js
+++ .release-it.js
@@ -1,17 +1,59 @@
-const CzConfig = require('./.cz-config.js');
-
 module.exports = {
   "plugins": {
     "@release-it/conventional-changelog": {
       "infile": "CHANGELOG.md",
       "preset": {
         "name": "conventionalcommits",
-        "types": CzConfig.types.map(type => {
-          return {
-            "type": type.value,
-            "section": type.name
+         types: [
+          {
+            type: ':sparkles: feat',
+            section: '✨ feat:     新功能'
+          },
+          {
+            type: ':bug: fix',
+            section: '🐛 fix:      修复 bug'
+          },
+          {
+            type: ':tada: init',
+            section: '🎉 init:     初始化'
+          },
+          {
+            type: ':memo: docs',
+            section: '📝 docs:     文档变更'
+          },
+          {
+            type: ':art: style',
+            section: '🎨 style:    代码样式美化'
+          },
+          {
+            type: ':recycle: refactor',
+            section: '♻️  refactor: 重构'
+          },
+          {
+            type: ':zap: perf',
+            section: '⚡️ perf:     性能优化'
+          },
+          {
+            type: ':white_check_mark: test',
+            section: '✅ test:     测试'
+          },
+          {
+            type: ':rewind: revert',
+            section: '⏪️ revert:   回退'
+          },
+          {
+            type: ':package: build',
+            section: '📦️ build:    打包'
+          },
+          {
+            type: ':rocket: chore',
+            section: '🚀 chore:    构建/工程依赖/工具'
+          },
+          {
+            type: ':construction_worker: ci',
+            section: '👷 ci:       CI 相关变更'
           }
-        })
+        ]
       }
     }
   },
