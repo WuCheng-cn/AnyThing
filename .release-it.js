2e3f953543eb181313fd588a9b8a4e89054d214e
wucheng <1965417564@qq.com>
2025-03-06 19:02:00 +0800

    :rocket: chore(custom): 配置变更

--- .release-it.js
+++ .release-it.js
@@ -4,56 +4,70 @@ module.exports = {
       "infile": "CHANGELOG.md",
       "preset": {
         "name": "conventionalcommits",
-         types: [
+        "types": [
           {
-            type: 'feat',
-            section: '✨ Features | 新功能'
+            "type": 'feat',
+            "section": '✨ Features | 新功能'
           },
           {
-            type: 'fix',
-            section: '🐛 Bug Fixes | 修复 bug'
+            "type": 'fix',
+            "section": '🐛 Bug Fixes | 修复 bug'
           },
           {
-            type: 'init',
-            section: '🎉 Init | 初始化'
+            "type": 'init',
+            "section": '🎉 Init | 初始化'
           },
           {
-            type: 'docs',
-            section: '📝 Documentation | 文档变更'
+            "type": 'docs',
+            "section": '📝 Documentation | 文档变更'
           },
           {
-            type: 'style',
-            section: '🎨 Styles | 代码样式美化'
+            "type": 'style',
+            "section": '🎨 Styles | 代码样式美化'
           },
           {
-            type: 'refactor',
-            section: '♻️ Code Refactoring | 重构'
+            "type": 'refactor',
+            "section": '♻️ Code Refactoring | 重构'
           },
           {
-            type: 'perf',
-            section: '⚡️ Performance Improvements | 性能优化'
+            "type": 'perf',
+            "section": '⚡️ Performance Improvements | 性能优化'
           },
           {
-            type: 'test',
-            section: '✅ Tests | 测试'
+            "type": 'test',
+            "section": '✅ Tests | 测试'
           },
           {
-            type: 'revert',
-            section: '⏪️ Reverts | 回退'
+            "type": 'revert',
+            "section": '⏪️ Reverts | 回退'
           },
           {
-            type: 'build',
-            section: '📦️ Builds | 打包'
+            "type": 'build',
+            "section": '📦️ Builds | 打包'
           },
           {
-            type: 'chore',
-            section: '🚀 Chores | 构建/工程依赖/工具'
+            "type": 'chore',
+            "section": '🚀 Chores | 构建/工程依赖/工具'
           },
           {
-            type: 'ci',
-            section: '👷 Continuous Integrations | CI 相关变更'
+            "type": 'ci',
+            "section": '👷 Continuous Integrations | CI 相关变更'
           }
         ]
+      },
+      "config": {
+        "parserOpts": {
+          "headerPattern": "^(:[a-z]+:)\\s*(\\w+)(\\(([^)]+)\\))?\\s*:\\s*(.*)$",
+          "headerCorrespondence": [
+            "emoji",
+            "type",
+            "scope",
+            "scope",
+            "subject"
+          ],
+          "noteKeywords": ["BREAKING CHANGE", "BREAKING CHANGES"],
+          "issuePrefixes": ["#"]
+        }
       }
     }
   },
@@ -67,4 +81,4 @@ module.exports = {
   "npm": {
     "publish": true
   }
-}
+};
